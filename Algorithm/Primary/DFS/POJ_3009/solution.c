9abc4587c9631032a1ad02393dd625ea281f3307
SIMPLIFY_HPEB <2585638549@qq.com>
2020-12-30 15:28:43 +0800

    a little change ;)

--- Algorithm/Primary/DFS/POJ_3009/solution.c
+++ Algorithm/Primary/DFS/POJ_3009/solution.c
@@ -1,75 +1,66 @@
 #include<stdio.h>
+#include<string.h>
 
-int dfs(int x, int y, int step);
+int board[20][20], exist_row[20], exist_line[20];
+int width, height, minimu_moves;
+int next_x[4] = { -1, 0, 1, 0 };
+int next_y[4] = { 0, -1, 0, 1 };
 
-int s_x, s_y, width, height, minimum_step, board[20][20];
+void dfs(int y, int x, int moves);
 
 int main()
 {
+	int x, y, s_x, s_y;
 	for (scanf("%d %d", &width, &height); width * height != 0; printf("\n"), scanf("%d %d", &width, &height)) {
-		minimum_step = 11;
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
+		minimu_moves = 11;
+		memset(exist_row, 0, sizeof(exist_row));
+		memset(exist_line, 0, sizeof(exist_line));
+		for (y = 0; y < height; y++) {
+			for (x = 0; x < width; x++) {
 				scanf("%d", &board[y][x]);
+				if (board[y][x] == 1 || board[y][x] == 3) {
+					exist_row[y] += 1;
+					exist_line[x] += 1;
+				}
 				if (board[y][x] == 2) {
-					board[y][x] = 0;
-					s_x = x, s_y = y;
+					s_x = x;
+					s_y = y;
 				}
 			}
 		}
-		dfs(s_x, s_y, 0);
-		if (minimum_step == 11)
-			minimum_step = -1;
-		printf("%d", minimum_step);
+		dfs(s_y, s_x, 1);
+		printf("%d", minimu_moves == 11 ? -1 : minimu_moves);
 	}
-}
 
-int dfs(int x, int y, int step)
+}
+void dfs(int y, int x, int moves)
 {
-	if (step > 10)
-		return -1;
-	for (int i = 1;x + i < width && board[y][x + i] != 1; i++) {
-		if (board[y][x + i] == 3) {
-			minimum_step = minimum_step > step + 1 ? step + 1 : minimum_step;
-			return minimum_step;
-		}
-		if (x + i + 1 < width && board[y][x + i + 1] == 1) {
-			board[y][x + i + 1] = 0;
-			dfs(x + i, y, step + 1);
-			board[y][x + i + 1] = 1;
-		}
+	int i, j, n_x, n_y, * e_p;
+	if (moves == 11) {
+		return;
 	}
-	for (int i = 1; x - i >= 0 && board[y][x - i] != 1; i++) {
-		if (board[y][x - i] == 3) {
-			minimum_step = minimum_step > step + 1 ? step + 1 : minimum_step;
-			return minimum_step;
+	for (i = 0; i < 4; i++) {
+		n_x = x;
+		n_y = y;
+		e_p = n_x + next_x[i] == x ? &exist_line[x] : &exist_row[y];
+		if (*e_p == 0) {
+			continue;
 		}
-		if (x - i - 1 >= 0 && board[y][x - i - 1] == 1) {
-			board[y][x - i - 1] = 0;
-			dfs(x - i, y, step + 1);
-			board[y][x - i - 1] = 1;
+		while (n_x + next_x[i] >= 0 && n_x + next_x[i] < width && n_y + next_y[i] >= 0 && n_y + next_y[i] < height && board[n_y + next_y[i]][n_x + next_x[i]] == 0) {
+			n_x += next_x[i];
+			n_y += next_y[i];
 		}
-	}
-	for (int i = 1; y + i < height; i++) {
-		if (board[y + i][x] == 3) {
-			minimum_step = minimum_step > step + 1 ? step + 1 : minimum_step;
-			return minimum_step;
-		}
-		if (y + i + 1 < height && board[y + i + 1][x] == 1) {
-			board[y + i + 1][x] = 0;
-			dfs(x, y + i, step + 1);
-			board[y + i + 1][x] = 1;
-		}
-	}
-	for (int i = 1; y - i >= 0; i++) {
-		if (board[y - i][x] == 3) {
-			minimum_step = minimum_step > step + 1 ? step + 1 : minimum_step;
-			return minimum_step;
-		}
-		if (y - i - 1 >= 0 && board[y - i - 1][x] == 1) {
-			board[y - i - 1][x] = 0;
-			dfs(x, y - i, step + 1);
-			board[y - i - 1][x] = 1;
+		if (n_x + next_x[i] >= 0 && n_x + next_x[i] < width && n_y + next_y[i] >= 0 && n_y + next_y[i] < height) {
+			if (board[n_y + next_y[i]][n_x + next_x[i]] == 1 && (n_x != x || n_y != y)) {
+				board[n_y + next_y[i]][n_x + next_x[i]] = 0;
+				*e_p -= 1;
+				dfs(n_y, n_x, ++moves);
+				board[n_y + next_y[i]][n_x + next_x[i]] = 1;
+				*e_p += 1;
+			}
+			if (board[n_y + next_y[i]][n_x + next_x[i]] == 3) {
+				minimu_moves = minimu_moves < moves ? minimu_moves : moves;
+			}
 		}
 	}
 }
